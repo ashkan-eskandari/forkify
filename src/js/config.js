7b2423c7af61a2ed4ae7eacc470216cef93e892f
ashkan-eskandari <ashkan.eskandari.dev@gmail.com>
2023-07-07 12:01:26 +0330

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '3078d590-e320-4017-9b51-e25218112d71';
+export const MODAL_CLOSE_SEC = 2.5;
